0ea6231aace70c4d0a685cd725b8069b04d7ee41
Hmutea <hmukimal@gmail.com>
2024-07-28 20:08:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
+global.AntiCallBot = false // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
 global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
@@ -19,7 +19,7 @@ global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chu
 global.owner = ['918837094883'] // I phone number in thlak rawh
 global.ownernumber = '918837094883' // I phone number in thlak rawh
 global.ownername = "꧁HKIMA42♕꧂" // I hming dah tel bawk la
-global.location = "India, Mizoram, KHAWZAWL" // I awmna hmun daha
+global.location = "India, Mizoram, Khawzawl vegthar" // I awmna hmun daha
 global.botname = "HKIMA_42" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
@@ -42,7 +42,7 @@ global.buttonigurl = "https://www.instagram.com/hkima_42?igsh=czNrbnptOG5odHR3"
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
 //messages
 global.mess = {
-    success: '\n*©HBWABot*!', 
+    success: '\n*©HKIMA_42*!', 
     admin: 'He features hi chu admin-te chauh hman thei tur a siam a ni!',
     botAdmin: 'Admin-ah min dah hmasa phawt la chuan ka ti thei ang!',
     owner: 'He features hi a neitu chauh hman thei tura siam a ni!',
